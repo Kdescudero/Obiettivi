feb0051c7f1068f724e3a5cd6e78677cf2051601
Kdescudero <kdescudero01@gmail.com>
2019-11-04 16:33:39 -0500

    Promer comit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native-ui-kitten'
+
+const App = () => {
+  return (
+    <>
+      <Text>Here</Text>
+    </>
+  );
+};
+
+export default App;
